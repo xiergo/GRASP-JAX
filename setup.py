e12a48348ba99496d1f8caf3f7be734f5e5be08c
xiergo <18811771366@163.com>
2025-03-03 09:36:14 +0800

    setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     long_description="Integrating Diverse Experimental Information to Assist Protein Complex Structure Prediction",
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=['py3Dmol','absl-py','biopython',
-                      'chex','dm-haiku','dm-tree',
-                      'immutabledict','jax','ml-collections',
-                      'numpy','pandas','scipy','optax','joblib',
-                      'matplotlib', 'scikit-learn', 'tqdm'],
+    install_requires=['py3Dmol','absl-py==1.0.0','biopython==1.79',
+                      'chex==0.1.86','dm-haiku==0.0.12','dm-tree==0.1.8',
+                      'immutabledict==2.0.0','ml-collections==0.1.0',
+                      'numpy==1.24.3','pandas==2.0.3','scipy==1.11.1',
+                      'matplotlib', 'scikit-learn==1.5.2'],
     include_package_data=True
 )
